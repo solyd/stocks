207a37354208a554e66f828cb88311fc88d11903
sol <alxndr.yav@gmail.com>
2014-11-13 21:24:36 +0200

    basic info on how to use yahoo finance

---
+++ stocks.lisp
@@ -0,0 +1,20 @@
+
+
+(ql:quickload "cl-csv")
+(ql:quickload "drakma")
+
+(setf *yahoo-api-base-url* "http://finance.yahoo.com/d/quotes.csv")
+(setf *companies* '("AAPL"
+                    "GOOG"
+                    "MSFT"))
+
+(setf csv-filepath (make-pathname :directory "/home/sol/" :name "quotes.csv"))
+(setf drakma:*header-stream* *standard-output*)
+
+(setf api-url "http://finance.yahoo.com/d/quotes.csv?s=AAPL+GOOG+MSFT&f=nab")
+
+(setf http-response (drakma:http-request api-url))
+(with-open-file (f "/home/sol/Downloads/tmp.csv"
+                   :direction :output
+                   :element-type '(unsigned-byte 8))
+  (write-sequence http-response f))
